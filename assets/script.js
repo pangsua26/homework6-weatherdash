4796d36c94d801df02b6077a9c4e64c987ea31bf
Pangsua Vang <pangsua26@yahoo.com>
2020-05-06 18:26:26 -0500

    added weather api

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,2 @@
 var APIkey = "e48e3b2c9156b8d1e78800b981022bbb";
 var weatherAPI = "api.openweathermap.org/data/2.5/weather?q=";
-var fiveDayForecast = "api.openweathermap.org/data/2.5/forecast?q=";
-var uvAPI = "http://api.openweathermap.org/data/2.5/uvi?lat=";
