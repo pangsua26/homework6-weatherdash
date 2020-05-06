48cb620511df4137b64f456292ebb6572f5d1387
Pangsua Vang <pangsua26@yahoo.com>
2020-05-06 18:27:02 -0500

    added 5 day forecast api call

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,3 @@
 var APIkey = "e48e3b2c9156b8d1e78800b981022bbb";
 var weatherAPI = "api.openweathermap.org/data/2.5/weather?q=";
+var fiveDayForecast = "api.openweathermap.org/data/2.5/forecast?q=";
