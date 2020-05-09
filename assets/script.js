05cbafbb6eeaa01b33a44d37d226469f90476869
Pangsua Vang <pangsua26@yahoo.com>
2020-05-09 18:40:22 -0500

    added city search array

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
-var APIkey = "e48e3b2c9156b8d1e78800b981022bbb";
-var weatherAPI = "api.openweathermap.org/data/2.5/weather?q=";
+var APIKey = "e48e3b2c9156b8d1e78800b981022bbb";
+var weatherAPI = "api.openweathermap.org/data/2.5/weather?"
 var fiveDayForecast = "api.openweathermap.org/data/2.5/forecast?q=";
 var uvAPI = "http://api.openweathermap.org/data/2.5/uvi?lat=";
-var searchCity;
+var searchCity = ["Minneapolis","Austin", "Chicago", "New York", "Orlando", "San Franciso"];
+
+
+
+
+
+
+
+
 
 
