428067e048e3078c79392317b0ae5dff32ade9d7
Pangsua Vang <pangsua26@yahoo.com>
2020-05-09 21:23:19 -0500

    added function to search array and display on console

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,26 @@ var searchCity = ["Minneapolis","Austin", "Chicago", "New York", "Orlando", "San
 var lon = "longitude";
 var lat = "latidtude";
 
+searchCity.forEach(function (city, index, originalArr) {
+    returnCity(city);
+
+    if (index === originalArr.length - 1) {
+        displayWeatherInfo(city);
+    }
+    console.log(city);
+})
+
 $(document).ready(function() {
     // function to add current day to city name container
     var currentDay = moment().format("dddd MMMM Do");
     $("#city-name").text(currentDay);
     // console.log(currentDay);
+        
 })
 
 function returnCity(city) {
     var button = $("<button>");
-    button.addClass("city-btn btn btn-default");
+    button.addClass("city-btn btn btn-default").css("display", "block");
     button.attr("data-name", city);
     button.text(city);
     $(".search-city-array").append(button);
@@ -30,10 +40,3 @@ $("#search-button").on("click", function (event) {
     localStorage.setItem("weather", JSON.stringify(cities))
           
 });
-
-
-
-
-
-
-
