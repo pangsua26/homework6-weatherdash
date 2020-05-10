6e5627d8b70114ce32d84e6638534b6783c4d360
Pangsua Vang <pangsua26@yahoo.com>
2020-05-09 23:20:49 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var searchCity = ["Minneapolis","Austin", "Chicago", "New York", "Orlando", "San
 var lon = "longitude";
 var lat = "latidtude";
 
+// function to display searchCity arrays
 searchCity.forEach(function (city, index, originalArr) {
     returnCity(city);
 
@@ -22,25 +23,33 @@ $(document).ready(function() {
     console.log(currentDay);   
 })
 
+// function to display city weather information
 function displayWeatherInfo(city) {
     var apiKey = "31be87001622c83535cd8f39b27eb25b";
 
-    // var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=imperial`;
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=imperial";
 
+    //start of call
     $.ajax({
         url: queryURL, 
         method: "GET"
+    // store 
     }).then(function (response){
-        var infoReturn = response;
-        console.log(infoReturn);
+        console.log(queryURL);
+        console.log(response);
+
+        // $("#temperature").text("Temperature (K) " + response.main.temp);
+        // $("#humidity")..text("Humidity: " + response.main.humidity);
+        // $("#wind-speed").text("Wind Speed: " + response.wind.speed);
         
+       
     })
 
 }
 
-
+// function to create buttons on city names to return weather information
 function returnCity(city) {
     var button = $("<button>");
     button.addClass("city-btn btn btn-default").css("display", "block");
@@ -49,14 +58,18 @@ function returnCity(city) {
     $(".search-city-array").append(button);
 }
 
+// Listerner on search button
 $("#search-button").on("click", function (event) {
     event.preventDefault();
 
+    // create new variable on search input
     var $weather = $("#search-input").val().trim();
 
+    // update the search history
     searchCity.push($weather);
     localStorage.setItem("weather", JSON.stringify(searchCity))
     
+    // call functions
     returnCity($weather);
     displayWeatherInfo($weather);
 
