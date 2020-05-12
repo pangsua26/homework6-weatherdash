5dfd8348b76cb74a8d5eeaf522cdbf3106f67e9f
Pangsua Vang <pangsua26@yahoo.com>
2020-05-12 14:16:09 -0500

    added start of uv function

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
-var fiveDayForecast = "api.openweathermap.org/data/2.5/forecast?q=";
+    var fiveDayForecast = "api.openweathermap.org/data/2.5/forecast?q=";
 
-var searchCity = ["Minneapolis","Austin", "Chicago", "New York", "Orlando", "San Franciso"];
-var lon = "longitude";
-var lat = "latidtude";
+    var searchCity = ["Minneapolis","Austin", "Chicago", "New York", "Orlando", "San Franciso"];
+    var lon = "longitude";
+    var lat = "latidtude";
 
 // function to display searchCity arrays
-searchCity.forEach(function (city, index, originalArr) {
-    returnCity(city);
+    searchCity.forEach(function (city, index, originalArr) {
+        returnCity(city);
 
-    if (index === originalArr.length - 1) {
-        displayWeatherInfo(city);
-    }
-    // console.log(city);
-})
+        if (index === originalArr.length - 1) {
+            displayWeatherInfo(city);
+        }
+        // console.log(city);
+    })
 
 
     // function to add current day to city name container
@@ -23,11 +23,11 @@ searchCity.forEach(function (city, index, originalArr) {
     // console.log(currentDay);   
 
 
-// function to display city weather information
-function displayWeatherInfo(city) {
-    var apiKey = "31be87001622c83535cd8f39b27eb25b";
+    // function to display city weather information
+    function displayWeatherInfo(city) {
+        var apiKey = "31be87001622c83535cd8f39b27eb25b";
 
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+        var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
     // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=imperial";
 
@@ -40,46 +40,56 @@ function displayWeatherInfo(city) {
         console.log(queryURL);
         console.log(response);
 
+        $("#city-name").text(response.name);
         $("#temperature").text("Temperature (K) " + response.main.temp);
         $("#humidity")..text("Humidity: " + response.main.humidity);
         $("#wind-speed").text("Wind Speed: " + response.wind.speed);
+
+        uvIndex(response.coord.lon,response.coord.lat);
         
        
     })
 
 }
 
-// function to create buttons on city names to return weather information
-function returnCity(city) {
-    var button = $("<button>");
-    button.addClass("city-btn btn btn-default").css("display", "block");
-    button.attr("data-name", city);
-    button.attr("value", city);
-    button.text(city);
-    $(".search-city-array").append(button);
-}
+    function uvIndex(lon, lat) {
+        var apiKey = "31be87001622c83535cd8f39b27eb25b";
+        var queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid="+ apiKey + "&lat="+ lat + "&lon=" + lon;
+        console.log(queryURL);
 
-$(".search-city-array").on("click", ".city-btn", function() {
-    var buttonVal = $(this).val().trim();
-    console.log(buttonVal);
-    displayWeatherInfo(buttonVal);
+    }
 
-})
+    // function to create buttons on city names to return weather information
+    function returnCity(city) {
+        var button = $("<button>");
+        button.addClass("city-btn btn btn-default").css("display", "block");
+        button.attr("data-name", city);
+        button.attr("value", city);
+        button.text(city);
+        $(".search-city-array").append(button);
+    }
 
-// Listerner on search button
-$("#search-button").on("click", function (event) {
-    event.preventDefault();
+    $(".search-city-array").on("click", ".city-btn", function() {
+        var buttonVal = $(this).val().trim();
+        console.log(buttonVal);
+        displayWeatherInfo(buttonVal);
 
-    // create new variable on search input
-    var $weather = $("#search-input").val().trim();
+    })
 
-    // update the search history
-    searchCity.push($weather);
-    localStorage.setItem("weather", JSON.stringify(searchCity))
-    
-    // call functions
-    returnCity($weather);
-    displayWeatherInfo($weather);
+    // Listerner on search button
+    $("#search-button").on("click", function (event) {
+        event.preventDefault();
 
-});
+        // create new variable on search input
+        var $weather = $("#search-input").val().trim();
+
+        // update the search history
+        searchCity.push($weather);
+        localStorage.setItem("weather", JSON.stringify(searchCity))
+        
+        // call functions
+        returnCity($weather);
+        displayWeatherInfo($weather);
+
+    });
 })
